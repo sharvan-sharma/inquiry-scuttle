ea656a146ed49b7cf7ee5e6c6b2243a632a705d6
sharvan-sharma <sharvansharma518@gmail.com>
2020-06-30 22:03:48 +0530

    minor filter changes

--- routes/middlewares/user_middlewares/filter.js
+++ routes/middlewares/user_middlewares/filter.js
@@ -1,8 +1,10 @@
-const {Inquiry} = require('../../../src/config/models')
+const {Projects,Inquiry, Forms} = require('../../../src/config/models')
 const {masterLogger} =require('../../../src/logger')
 
-module.exports = (req,res,next) => {
-    if(!req.body.formsIdArray || !Array.isArray(req.body.formsIdArray)){
+async function filter (req,res,next){
+    if(req.body.all === undefined){
+        res.json({status:423,type:'all'})
+    }else if(req.body.all === false && (!req.body.formsIdArray || !Array.isArray(req.body.formsIdArray))){
         res.json({status:423,type:'formsIdArray'})
     }else if(!req.body.read || !Array.isArray(req.body.read)){
         res.json({status:423,type:'read'})
@@ -13,28 +15,61 @@ module.exports = (req,res,next) => {
     }else if(req.body.maxdate === undefined){
         res.json({status:423,type:'maxdate'})
     }else{
-        let {formsIdArray,read,replied,mindate,maxdate} = req.body
+        let {all,formsIdArray,read,replied,mindate,maxdate} = req.body
         
         mindate = (mindate === null)?new Date(req.user.createdAt):new Date(mindate)
         maxdate = (maxdate === null)?new Date():new Date(maxdate)
       
-        Inquiry.find(
-            {
-                $and:[
-                    {form_id:{$in:formsIdArray}},
-                    {read:{$in:read}},
-                    {replied:{$in:replied}},
-                    {createdAt:{$gte:mindate,$lt:maxdate}}
-                ]
-            }
-        ).sort({createdAt:-1})
-        .exec((err,array)=>{
-            if(err){
+        if(all === true){
+            try{
+                const projectsObjectArray = await Projects.find({user_id:req.user._id},{_id:1})
+                const projectsArray = projectsObjectArray.map(ele=>ele._id)
+                const formsObjectArray = await Forms.find({project_id:{$in:projectsArray}},{_id:1})
+                const formsArray = formsObjectArray.map(ele=>ele._id)
+
+                Inquiry.find(
+                    {
+                        $and:[
+                            {form_id:{$in:formsArray}},
+                            {read:{$in:read}},
+                            {replied:{$in:replied}},
+                            {createdAt:{$gte:mindate,$lt:maxdate}}
+                        ]
+                    }
+                ).sort({createdAt:-1})
+                .exec((err,array)=>{
+                    if(err){
+                        res.json({status:500})
+                        masterLogger.error(`user ${req.user.email} error while using filter for inquiries`)
+                    }else{
+                        res.json({status:200,array})
+                    }
+                })
+            }catch(e){
                 res.json({status:500})
                 masterLogger.error(`user ${req.user.email} error while using filter for inquiries`)
-            }else{
-                res.json({status:200,array})
             }
-        })
+        }else{
+            Inquiry.find(
+                {
+                    $and:[
+                        {form_id:{$in:formsIdArray}},
+                        {read:{$in:read}},
+                        {replied:{$in:replied}},
+                        {createdAt:{$gte:mindate,$lt:maxdate}}
+                    ]
+                }
+            ).sort({createdAt:-1})
+            .exec((err,array)=>{
+                if(err){
+                    res.json({status:500})
+                    masterLogger.error(`user ${req.user.email} error while using filter for inquiries`)
+                }else{
+                    res.json({status:200,array})
+                }
+            })
+        }
     }
 }
+
+module.exports = filter
