99e8ffb32f4412723858b9906694f6f58250cee7
sharvan-sharma <sharvansharma518@gmail.com>
2020-07-01 18:39:40 +0530

    po-d-1

--- src/utils/sites.js
+++ src/utils/sites.js
@@ -2,9 +2,14 @@ const sitesArray = [
     'http://localhost:3000',
     'http://localhost:3001',
     'https://salesicon-crm.web.app',
+    'https://salesicon-crm.firebaseapp.com',
     'https://noteskeeper-md.web.app',
+    'https://noteskeeper-md.firebaseapp.com',
     'https://crypt-oauth.web.app',
-    'https://crypt-docs.netlify.com'
+    'https://crypt-oauth.firebaseapp.com',
+    'https://crypt-docs.netlify.com',
+    'https://inquiryscuttle.web.app',
+    'https://inquiryscuttle.firebaseapp.com'
 ]
 
 module.exports = sitesArray
